7da684b36190bcd2e814206bbd52f41390d4a02f
GlebDik <112880979+GlebDik@users.noreply.github.com>
2023-04-03 17:11:30 +0300

    bebera

--- main.py
+++ main.py
@@ -4,22 +4,19 @@ import time
 import random
 
 pygame.init()
-pink = (255, 192, 203)
 white = (255, 255, 255)
-yellow = (255, 255, 102)
 black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 153, 0)
-blue = (50, 153, 213)
 dis_width = 800
-dis_height = 600
+dis_height = 500
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Антивирусник')
 
 
 clock = pygame.time.Clock()
 snake_block = 10
-snake_speed = 15
+snake_speed = 13
 font_style = pygame.font.SysFont("Antropos Freefont", 25)
 score_font = pygame.font.SysFont("Antropos Freefont", 25)
 
