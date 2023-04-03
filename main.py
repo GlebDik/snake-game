fe8d7902ef9ee74ebcb7ef614c0b53610ffe0659
GlebDik <112880979+GlebDik@users.noreply.github.com>
2023-04-03 16:19:00 +0300

    правки2

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+import pygame
+import sys
+import time
+import random
+
+pygame.init()
+pink = (255, 192, 203)
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 153, 0)
+blue = (50, 153, 213)
+dis_width = 800
+dis_height = 600
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Антивирусник')
+
+
+clock = pygame.time.Clock()
+snake_block = 10
+snake_speed = 15
+font_style = pygame.font.SysFont("Antropos Freefont", 25)
+score_font = pygame.font.SysFont("Antropos Freefont", 25)
+
+
+def Your_score(score):
+    value = score_font.render("Вирусов удалено: " + str(score), True, green)
+    dis.blit(value, [0, 0])
 
 
 def our_snake(snake_block, snake_list):
